72e2558f3c063b75bd508574e17215fe644f0262
JasonNing <898888121@qq.com>
2019-05-24 21:44:18 +0800

    http 请求挂载在vue上

--- aboot_admin/src/main.js
+++ aboot_admin/src/main.js
@@ -1,9 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router/index'
-import store from './store'
+import store from './store/index'
+import { getRequest, post2json, post2Form } from './utils/api';
 
 Vue.config.productionTip = false
+
+//http请求挂载vue原型
+Vue.propotype.get = getRequest;
+Vue.propotype.post2json = post2json;
+Vue.propotype.post2Form = post2Form;
 
 new Vue({
   router,
